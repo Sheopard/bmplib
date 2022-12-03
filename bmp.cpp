d429daac834935375b9e536c2bd56d90a197320b
Sheopard <sirexword@gmail.com>
2022-12-03 03:55:27 +0300

    Start bmplib rework

--- bmp.cpp
+++ bmp.cpp
@@ -1,186 +1,271 @@
 #include "bmp.h"
 
-BMP24::BMP24(std::string filename) {
+BMP24::BMP24(const std::string& filename) {
 	this->load(filename);
 }
 
-void BMP24::write(std::string filename, std::vector<std::vector<RGBTRIPLE>>& dest) {
-	FILE *fout;
-	fopen_s(&fout, filename.c_str(), "wb");
-	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
-	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+//void BMP24::write(std::string filename, std::vector<std::vector<RGBTRIPLE>>& dest) {
+//	FILE *fout;
+//	fopen_s(&fout, filename.c_str(), "wb");
+//	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
+//	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+//
+//	int padding = 4 - (iHeader.width * 3) % 4;
+//	for (uint32_t i = 0; i < iHeader.height; i++) {
+//		for (uint32_t j = 0; j < iHeader.width; j++) {
+//			fwrite(&dest[i][j], sizeof(RGBTRIPLE), 1, fout);
+//		}
+//
+//		if (padding != 4) {
+//			fwrite(0, 1, padding, fout);
+//		}
+//	}
+//
+//	fclose(fout);
+//}
+
+void BMP24::write(const std::string& filename) {
+	std::ofstream fout(filename, std::ios_base::binary);
+
+	fout.write(reinterpret_cast<char*>(&fHeader), sizeof(fHeader) - 2);
+	fout.write(reinterpret_cast<char*>(&iHeader), sizeof(iHeader));	
 
 	int padding = 4 - (iHeader.width * 3) % 4;
-	for (uint32_t i = 0; i < iHeader.height; i++) {
-		for (uint32_t j = 0; j < iHeader.width; j++) {
-			fwrite(&dest[i][j], sizeof(RGBTRIPLE), 1, fout);
+	for (uint32_t i = 0; i < iHeader.width; ++i) {
+		for (uint32_t j = 0; j < iHeader.width; ++j) {
+			fout.write(reinterpret_cast<char*>(&pixels[i][j]), sizeof(RGBTRIPLE));
 		}
 
 		if (padding != 4) {
-			fwrite(0, 1, padding, fout);
+			fout.write(0, 1);
 		}
 	}
-
-	fclose(fout);
+	fout.close();
 }
 
-void BMP24::load(std::string& filename) {
-	FILE *fin;
-	fopen_s(&fin, filename.c_str(), "rb");
-
-	if (fin == NULL) {
+void BMP24::load(const std::string& filename) {
+	std::ifstream fin(filename, std::ios_base::binary);
+	if (!fin.is_open()) {
 		throw std::ios_base::failure("Error: Image not open");
 	}
 
-	fread(&fHeader, sizeof(fHeader) - 2, 1, fin);
-	fread(&iHeader, sizeof(iHeader), 1, fin);
+
+	fin.read(reinterpret_cast<char*>(&fHeader), sizeof(fHeader) - 2);
+	fin.read(reinterpret_cast<char*>(&iHeader), sizeof(iHeader));
+
 
 	pixels.resize(iHeader.height);
-	for (uint32_t i = 0; i < iHeader.height; i++) {
+	for (uint32_t i = 0; i < iHeader.height; ++i) {
 		pixels[i].resize(iHeader.width);
 	}
 
 	int padding = 4 - (iHeader.width * 3) % 4;
-	for (uint32_t i = 0; i < iHeader.height; i++) {
-		for (uint32_t j = 0; j < iHeader.width; j++) {
-			fread(&pixels[i][j], sizeof(RGBTRIPLE), 1, fin);
+	for (uint32_t i = 0; i < iHeader.height; ++i) {
+		for (uint32_t j = 0; j < iHeader.width; ++j) {
+			fin.read(reinterpret_cast<char*>(&pixels[i][j]), sizeof(RGBTRIPLE));
 		}
 
 		if (padding != 4) {
-			fseek(fin, padding, SEEK_CUR);
+			fin.seekg(padding, std::ios_base::cur);
 		}
 	}
+	fin.close();
 
-	fclose(fin);
+//	FILE *fin;
+//	fopen_s(&fin, filename.c_str(), "rb");
+//
+//	if (fin == NULL) {
+//		throw std::ios_base::failure("Error: Image not open");
+//	}
+//
+//	fread(&fHeader, sizeof(fHeader) - 2, 1, fin);
+//	fread(&iHeader, sizeof(iHeader), 1, fin);
+//
+//	pixels.resize(iHeader.height);
+//	for (uint32_t i = 0; i < iHeader.height; i++) {
+//		pixels[i].resize(iHeader.width);
+//	}
+//
+//	int padding = 4 - (iHeader.width * 3) % 4;
+//	for (uint32_t i = 0; i < iHeader.height; i++) {
+//		for (uint32_t j = 0; j < iHeader.width; j++) {
+//			fread(&pixels[i][j], sizeof(RGBTRIPLE), 1, fin);
+//		}
+//
+//		if (padding != 4) {
+//			fseek(fin, padding, SEEK_CUR);
+//		}
+//	}
+//
+//	fclose(fin);
 }
 
-void BMP24::to_bmp8(std::string& filename) {
-	int dest_wb = ((iHeader.width * 8 + 31) / 32) * 4;
-	int dest_size = dest_wb * iHeader.height;
-
-	fHeader.size = 54 + 4 * 256 + dest_size;
-	iHeader.bitCount = 8;
-	iHeader.colorUsed = 256;
-	iHeader.colorImportant = 0;
-	iHeader.sizeImage = dest_size;
-
-	std::vector<RGBTRIPLE> pallete(256);
-	for (uint32_t i = 0; i < 256; i++) {
-		pallete[i].red = pallete[i].blue = pallete[i].green = (uint8_t)i;
-	}
-
-	std::vector<uint8_t> dest(dest_size);
-
-	uint8_t blue = 0, red = 0, green = 0;
-	for (int i = iHeader.height - 1; i >= 0; i--) {
-		for (int j = 0; j < iHeader.width; j++) {
-			dest[i * dest_wb + j] = 0.299 * pixels[i][j].red + 0.587 * pixels[i][j].green + 0.114 * pixels[i][j].blue;
-		}
-	}
-
-	FILE *fout;
-	fopen_s(&fout, filename.c_str(), "wb");
-	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
-	fwrite(&iHeader, sizeof(iHeader), 1, fout);
-	for (int i = 0; i < 256; i++) {
-		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
-	}
-	for (int i = 0; i < dest_size; i++) {
-		fwrite(&dest[i], 1, 1, fout);
-	}
-
-	fclose(fout);
-}
-
-std::vector<std::vector<int>> BMP24::get_component(char color) {
-	std::vector<std::vector<int>> result(pixels.size(), std::vector<int>(pixels[0].size()));
-
-	if (color == 'r') {
-		for (uint32_t i = 0; i < iHeader.height; i++) {
-			for (uint32_t j = 0; j < iHeader.width; j++) {
-				result[i][j] = pixels[i][j].red;
-			}
-		}
-	}
-	else if (color == 'g') {
-		for (uint32_t i = 0; i < iHeader.height; i++) {
-			for (uint32_t j = 0; j < iHeader.width; j++) {
-				result[i][j] = pixels[i][j].green;
-			}
-		}
-	}
-	else if (color == 'b') {
-		for (uint32_t i = 0; i < iHeader.height; i++) {
-			for (uint32_t j = 0; j < iHeader.width; j++) {
-				result[i][j] = pixels[i][j].blue;
-			}
-		}
-	}
-
-	return result;
-}
+//void BMP24::to_bmp8(std::string& filename) {
+//	int dest_wb = ((iHeader.width * 8 + 31) / 32) * 4;
+//	int dest_size = dest_wb * iHeader.height;
+//
+//	fHeader.size = 54 + 4 * 256 + dest_size;
+//	iHeader.bitCount = 8;
+//	iHeader.colorUsed = 256;
+//	iHeader.colorImportant = 0;
+//	iHeader.sizeImage = dest_size;
+//
+//	std::vector<RGBTRIPLE> pallete(256);
+//	for (uint32_t i = 0; i < 256; i++) {
+//		pallete[i].red = pallete[i].blue = pallete[i].green = (uint8_t)i;
+//	}
+//
+//	std::vector<uint8_t> dest(dest_size);
+//
+//	uint8_t blue = 0, red = 0, green = 0;
+//	for (int i = iHeader.height - 1; i >= 0; i--) {
+//		for (int j = 0; j < iHeader.width; j++) {
+//			dest[i * dest_wb + j] = 0.299 * pixels[i][j].red + 0.587 * pixels[i][j].green + 0.114 * pixels[i][j].blue;
+//		}
+//	}
+//
+//	FILE *fout;
+//	fopen_s(&fout, filename.c_str(), "wb");
+//	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
+//	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+//	for (int i = 0; i < 256; i++) {
+//		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
+//	}
+//	for (int i = 0; i < dest_size; i++) {
+//		fwrite(&dest[i], 1, 1, fout);
+//	}
+//
+//	fclose(fout);
+//}
+//
+//std::vector<std::vector<int>> BMP24::get_component(char color) {
+//	std::vector<std::vector<int>> result(pixels.size(), std::vector<int>(pixels[0].size()));
+//
+//	if (color == 'r') {
+//		for (uint32_t i = 0; i < iHeader.height; i++) {
+//			for (uint32_t j = 0; j < iHeader.width; j++) {
+//				result[i][j] = pixels[i][j].red;
+//			}
+//		}
+//	}
+//	else if (color == 'g') {
+//		for (uint32_t i = 0; i < iHeader.height; i++) {
+//			for (uint32_t j = 0; j < iHeader.width; j++) {
+//				result[i][j] = pixels[i][j].green;
+//			}
+//		}
+//	}
+//	else if (color == 'b') {
+//		for (uint32_t i = 0; i < iHeader.height; i++) {
+//			for (uint32_t j = 0; j < iHeader.width; j++) {
+//				result[i][j] = pixels[i][j].blue;
+//			}
+//		}
+//	}
+//
+//	return result;
+//}
 
 // ==================================================================
 
-BMP8::BMP8(std::string filename) {
+BMP8::BMP8(const std::string& filename) {
 	this->load(filename);
 }
 
-void BMP8::load(std::string filename) {
-	FILE *fin;
-	fopen_s(&fin, filename.c_str(), "rb");
-
-	if (fin == NULL) {
+void BMP8::load(const std::string& filename) {
+	std::ifstream fin(filename, std::ios::binary);
+	if (!fin.is_open()) {
 		throw std::ios_base::failure("Error: Image not open");
 	}
 
-	fread(&fHeader, sizeof(fHeader) - 2, 1, fin);
-	fread(&iHeader, sizeof(iHeader), 1, fin);
+	fin.read(reinterpret_cast<char*>(&fHeader), sizeof(fHeader) - 2);
+	fin.read(reinterpret_cast<char*>(&iHeader), sizeof(iHeader));
 
 	pallete.resize(256);
-	for (uint32_t i = 0; i < 256; i++) {
-		fread(&pallete[i], sizeof(RGBTRIPLE), 1, fin);
-		fseek(fin, 1, SEEK_CUR);
+	for (uint16_t i = 0; i < 256; ++i) {
+		fin.read(reinterpret_cast<char*>(&pallete[i]), sizeof(RGBTRIPLE));
+		fin.seekg(1, std::ios_base::cur);
 	}
-
+	
 	pixels.resize(iHeader.width * iHeader.height);
-	for (int i = 0; i < pixels.size(); i++) {
-		fread(&pixels[i], 1, 1, fin);
+	const int size = pixels.size();
+	for (uint16_t i = 0; i < size; ++i) {
+		fin.read(reinterpret_cast<char*>(&pixels[i]), 1);
 	}
+	
+	fin.close();
 
-	fclose(fin);
+//	FILE *fin;
+//	fopen_s(&fin, filename.c_str(), "rb");
+//
+//	if (fin == NULL) {
+//		throw std::ios_base::failure("Error: Image not open");
+//	}
+//
+//	fread(&fHeader, sizeof(fHeader) - 2, 1, fin);
+//	fread(&iHeader, sizeof(iHeader), 1, fin);
+//
+//	pallete.resize(256);
+//	for (uint32_t i = 0; i < 256; i++) {
+//		fread(&pallete[i], sizeof(RGBTRIPLE), 1, fin);
+//		fseek(fin, 1, SEEK_CUR);
+//	}
+//
+//	pixels.resize(iHeader.width * iHeader.height);
+//	for (int i = 0; i < pixels.size(); i++) {
+//		fread(&pixels[i], 1, 1, fin);
+//	}
+//
+//	fclose(fin);
 }
 
-void BMP8::write(std::string filename, std::vector<uint8_t>& pix) {
-	FILE *fout;
-	fopen_s(&fout, filename.c_str(), "wb");
-	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
-	fwrite(&iHeader, sizeof(iHeader), 1, fout);
-
-	for (int i = 0; i < 256; i++) {
-		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
-	}
-
-	for (int i = 0; i < pix.size(); i++) {
-		fwrite(&pix[i], 1, 1, fout);
-	}
-
-	fclose(fout);
+void BMP8::write(const std::string& filename, std::vector<uint8_t>& pix) {
+//	FILE *fout;
+//	fopen_s(&fout, filename.c_str(), "wb");
+//	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
+//	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+//
+//	for (int i = 0; i < 256; i++) {
+//		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
+//	}
+//
+//	for (int i = 0; i < pix.size(); i++) {
+//		fwrite(&pix[i], 1, 1, fout);
+//	}
+//
+//	fclose(fout);
 }
 
-void BMP8::write(std::string filename) {
-	FILE *fout;
-	fopen_s(&fout, filename.c_str(), "wb");
-	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
-	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+void BMP8::write(const std::string& filename) {
+	std::ofstream fout(filename, std::ios_base::binary);
+	
+	fout.write(reinterpret_cast<char*>(&fHeader), sizeof(fHeader) - 2);
+	fout.write(reinterpret_cast<char*>(&iHeader), sizeof(iHeader));
 
-	for (int i = 0; i < 256; i++) {
-		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
+	for (uint16_t i = 0; i < 256; ++i) {
+		fout.write(reinterpret_cast<char*>(&pallete[i]), sizeof(RGBTRIPLE));
 	}
 
-	for (int i = 0; i < pixels.size(); i++) {
-		fwrite(&pixels[i], 1, 1, fout);
+	const int size = pixels.size();
+	for (uint16_t i = 0; i < size; ++i) {
+		fout.write(reinterpret_cast<char*>(&pixels[i]), 1);
 	}
 
-	fclose(fout);
+	fout.close();
+
+
+//	FILE *fout;
+//	fopen_s(&fout, filename.c_str(), "wb");
+//	fwrite(&fHeader, sizeof(fHeader) - 2, 1, fout);
+//	fwrite(&iHeader, sizeof(iHeader), 1, fout);
+//
+//	for (int i = 0; i < 256; i++) {
+//		fwrite(&pallete[i], sizeof(RGBTRIPLE) + 1, 1, fout);
+//	}
+//
+//	for (int i = 0; i < pixels.size(); i++) {
+//		fwrite(&pixels[i], 1, 1, fout);
+//	}
+//
+//	fclose(fout);
 }
--- bmp.h
+++ bmp.h
@@ -2,71 +2,78 @@
 #define BMP_IMAGE_H_
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-struct BITMAPINFOHEADER {
-	int size;
-	int width;
-	int height;
-	short int planes;
-	short int bitCount;
-	int compression;
-	int sizeImage;
-	int XPelsPerMeter;
-	int YPelsPerMeter;
-	int colorUsed;
-	int colorImportant;
-};
+struct Image {
+	struct BITMAPINFOHEADER {
+		int size;
+		int width;
+		int height;
+		short int planes;
+		short int bitCount;
+		int compression;
+		int sizeImage;
+		int XPelsPerMeter;
+		int YPelsPerMeter;
+		int colorUsed;
+		int colorImportant;
+	};
 
-struct BITMAPFILEHEADER {
-	short int type;
-	int size;
-	short int reserved1;
-	short int reserved2;
-	int offsetBits;
-};
+	struct BITMAPFILEHEADER {
+		short int type;
+		int size;
+		short int reserved1;
+		short int reserved2;
+		int offsetBits;
+	};
 
-struct RGBTRIPLE {
-	unsigned char blue;
-	unsigned char green;
-	unsigned char red;
-};
-
-class BMP8 {
-private:
+	struct RGBTRIPLE {
+		uint8_t blue;
+		uint8_t green;
+		uint8_t red;
+	};
+	
 	BITMAPFILEHEADER fHeader;
 	BITMAPINFOHEADER iHeader;
+
+	virtual ~Image() {};
+public:
+	virtual void write(const std::string& filename) = 0;
+private:
+	virtual void load(const std::string& filename) = 0;	
+};
+
+
+class BMP8 : protected Image {
+private:
 	std::vector<uint8_t> pixels;
 	std::vector<RGBTRIPLE> pallete;
 public:
-	BMP8(std::string filename);
-	void write(std::string filename, std::vector<unsigned char>& pix);
-	void write(std::string filename);
-	std::vector<uint8_t> get_pixels() {
-		return pixels;
-	}
+	BMP8(const std::string& filename);
+	void write(const std::string& filename, std::vector<unsigned char>& pix);
+	void write(const std::string& filename) override final;
 private:
-	void load(std::string filename);
+	void load(const std::string& filename) override final;
 };
 
-class BMP24 {
+class BMP24 : protected Image {
 private:
-	BITMAPFILEHEADER fHeader;
-	BITMAPINFOHEADER iHeader;
-	std::vector<std::vector<RGBTRIPLE>> pixels;
-
+  	std::vector<std::vector<RGBTRIPLE>> pixels;
+  
 public:
-	BMP24(std::string filename);
-	void write(std::string filename, std::vector<std::vector<RGBTRIPLE>>& dest);
-	int width() { return iHeader.width; };
-	int height() { return iHeader.height; };
-	RGBTRIPLE get_pixel(int i, int j) { return pixels[j][i]; };
-	std::vector<std::vector<RGBTRIPLE>> get_pixels() { return pixels; };
-	std::vector<std::vector<int>> get_component(char color);
-	void to_bmp8(std::string& filename);
-
+  	BMP24(const std::string& filename);
+	void write(const std::string& filename) override final;
+  	void write(const std::string& filename, std::vector<std::vector<RGBTRIPLE>>& dest);
+  	//int width() { return iHeader.width; };
+  	//int height() { return iHeader.height; };
+  	//RGBTRIPLE get_pixel(int i, int j) { return pixels[j][i]; };
+  	//std::vector<std::vector<RGBTRIPLE>> get_pixels() { return pixels; };
+  	//std::vector<std::vector<int>> get_component(char color);
+  	//void to_bmp8(std::string& filename);
+  
 private:
-	void load(std::string& filename);
+	void load(const std::string& filename) override final;
 };
 
 #endif // !BMP_IMAGE_H_
