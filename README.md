6ac20ef6d553f51e6204e77758b5736591bc62b4
Sheopard <sirexword@gmail.com>
2022-12-10 01:37:07 +0300

    Reworked repository structure. Added README file. Added CMake support

---
+++ README.md
@@ -0,0 +1,28 @@
+# BMPLIB: A simple library for working with BMP files
+
+---
+
+To get started, follow these steps:
+
+## 1. Clone the repository
+
+```
+git clone https://github.com/Sheopard/bmplib.git
+
+or
+
+git clone git@github.com:Sheopard/bmplib.git
+```
+
+## 2. Run the bash script
+
+```
+bash build.sh
+```
+
+## 3. Run the executable file
+
+```
+./build/bmp.exe
+```
+
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <memory>
+
+#include "src/bmp_exceptions.h"
+#include "src/bmp.h"
+
+using namespace Images;
+
+int main(int argc, char* argv[]) {
+
+	/* BMP24 example */
+	std::unique_ptr<BMP24> image = std::make_unique<BMP24>("in.bmp");	
+	image->write("out.bmp");
+	
+	/* BMP24 to BMP8 example */
+	std::unique_ptr<BMP8> image8 = std::make_unique<BMP8>(image->grayscale());
+	image8->write("out8.bmp");
+
+	return 0;
+}
+
